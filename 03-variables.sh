f4e2b9ec94aeea55458f29f6d4bfa610d595b5eb
sukeerthpilla <sukeerth.unix@gmail.com>
2025-05-15 19:11:49 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing?"
+echo "$PERSON1: I am fine too. What's up?"
+echo "$PERSON2:: Nothing, Just going to mars now, Will you come?"
+echo "$PERSON1: Sorry, You carry on! I will come once you come back"
