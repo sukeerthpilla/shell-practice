3bc77b3d07efdd2a648b56cd36ed0f2c2d2c6345
sukeerthpilla <sukeerth.unix@gmail.com>
2025-05-15 18:37:32 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: Hey Musk, How are you?"
+echo "Musk:: Hello Trump, I am fine. How are you doing?"
+echo "Trump: I am fine too. What's up?"
+echo "Musk:: Nothing, Just going to mars now, Will you come?"
+echo "Trump: Sorry, You carry on! I will come once you come back"
