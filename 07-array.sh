def4d75832df866510d91eb9239a501f11286389
sukeerthpilla <sukeerth.unix@gmail.com>
2025-05-16 18:05:10 +0530

    hello world started

--- 07-array.sh
+++ 07-array.sh
@@ -2,7 +2,5 @@
 
 MOVIES=("court" "HIT3" "PUSHPA-2" "THANDEL")
 
-echo "First Movie: ${MOVIES(1)}"
-echo "First Movie: ${MOVIES(3)}"
-
-
+echo "First Movie: ${MOVIES[1]}"
+echo "First Movie: ${MOVIES[3]}"
